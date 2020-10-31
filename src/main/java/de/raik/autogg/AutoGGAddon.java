2a11905520b8704d069ab728b76ccb915b268913
MineFlash07 <mineflash07@gmail.com>
2020-10-31 22:18:27 +0100

    created addon basics

---
+++ src/main/java/de/raik/autogg/AutoGGAddon.java
@@ -0,0 +1,32 @@
+package de.raik.autogg;
+
+import net.labymod.api.LabyModAddon;
+import net.labymod.settings.elements.SettingsElement;
+
+import java.util.List;
+
+/**
+ * Addon class
+ * to handle the addon and
+ * its instance
+ *
+ * @author Raik
+ * @version 1.0
+ */
+public class AutoGGAddon extends LabyModAddon {
+
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void loadConfig() {
+
+    }
+
+    @Override
+    protected void fillSettings(List<SettingsElement> list) {
+
+    }
+}
